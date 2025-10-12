8a62316dc3939890c7dc32c4d69d3686e509405b
Caroline Contreras <carojanette2@gmail.com>
2025-10-12 15:05:51 -0700

    committing in case I mess up the camera implementation

--- dataworks/src/db.jsx
+++ dataworks/src/db.jsx
@@ -11,8 +11,13 @@ export function initDb() {
   try {
     db.execSync("PRAGMA journal_mode = WAL;");
   } catch {}
+
   try {
     db.execSync("PRAGMA busy_timeout = 5000;");
+  } catch {}
+
+  try {
+    db.execSync("PRAGMA foreign_keys = ON;");
   } catch {}
 
   db.withTransactionSync(() => {
@@ -21,14 +26,92 @@ export function initDb() {
         id   INTEGER PRIMARY KEY AUTOINCREMENT,
         tag  TEXT UNIQUE NOT NULL,
         name TEXT NOT NULL,
-        room_tag INTEGER NOT NULL
+        dept_id INTEGER,
+        serial TEXT NOT NULL,
+        po TEXT,
+        model TEXT,
+        manufacturer TEXT,
+        room_tag INTEGER,
+        type TEXT,
+        bus_unit TEXT CHECK(bus_unit IN ('BKCMP', 'BKASI', 'BKSTU', 'BKFDN', 'BKSPA')) DEFAULT 'BKCMP',
+        status TEXT,
+        assigned_to TEXT,
+        purchase_date TEXT,
+        geo_x TEXT,
+        geo_y TEXT,
+        elevation TEXT,
+        price REAL,
+        notes TEXT,
+        FOREIGN KEY (room_tag) REFERENCES room(id) ON DELETE SET NULL ON UPDATE CASCADE,
+        UNIQUE(tag),
+        FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE SET NULL ON UPDATE CASCADE
       );
+      CREATE INDEX IF NOT EXISTS idx_asset_tag ON asset_table (tag);
+      CREATE INDEX IF NOT EXISTS idx_asset_name ON asset_table (name);
+
+    `);
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS auth (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
+        username  TEXT UNIQUE NOT NULL,
+        email TEXT NOT NULL CHECK(email LIKE '%@csub.edu'),
+        pw TEXT,
+        school_id TEXT,
+        dept_id TEXT,
+        form_id TEXT,
+        signature TEXT,
+        f_name TEXT,
+        l_name TEXT,
+        role TEXT CHECK(role IN ('admin', 'user', 'management', 'custodian', 'student')) NOT NULL DEFAULT 'user',
+        kuali_key TEXT,
+        is_active INTEGER NOT NULL DEFAULT 1,
+        UNIQUE(username, email)
+      );
+      CREATE INDEX IF NOT EXISTS idx_auth_email ON auth (email);
+    `);
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS department (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        dept_id INTEGER NOT NULL,
+        manager TEXT,
+        custodian TEXT
+      );
+      CREATE INDEX IF NOT EXISTS idx_department_dept_id ON department (dept_id);
+    `);
+
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS department_cust (
+        custodian TEXT,
+        dept_id INTEGER,
+        FOREIGN KEY (dept_id) REFERENCES department(dept_id) ON DELETE CASCADE ON UPDATE CASCADE
+      );
+      CREATE INDEX IF NOT EXISTS idx_department_dept_id ON department (dept_id);
+    `);
+
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS building (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        bldg_id INTEGER NOT NULL
+      );
+      CREATE INDEX IF NOT EXISTS idx_building_bldg_id ON building (bldg_id);
+    `);
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS room (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        room_id INTEGER NOT NULL,
+        number TEXT,
+        bldg_id INTEGER NOT NULL,
+        FOREIGN KEY (bldg_id) REFERENCES building(id) ON DELETE CASCADE ON UPDATE CASCADE
+      );
+      CREATE INDEX IF NOT EXISTS idx_room_id ON room (room_id);
     `);
   });
 
   initialized = true;
 }
-
 // Insert or update (avoid crashing on duplicate tags)
 export function insertItem(tag, name, room_tag) {
   initDb();
